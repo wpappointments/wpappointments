ca36b37185dec22af7bed212a86f9492ad6ae991
Dawid Urbański <hi@dawidurbanski.com>
2023-12-22 10:34:02 +0100

    Move new component form to separate file

--- assets/scripts/admin/pages/Dashboard.tsx
+++ assets/scripts/admin/pages/Dashboard.tsx
@@ -1,18 +1,13 @@
 import { Button, Card, CardBody, CardHeader } from '@wordpress/components';
 import { select, useDispatch, useSelect } from '@wordpress/data';
 import { useState } from '@wordpress/element';
-import { useForm } from 'react-hook-form';
 import Table from '~/admin/components/Table/Table';
 import LayoutDefault from '~/admin/layouts/LayoutDefault/LayoutDefault';
 import SlideOut from '~/admin/components/SlideOut/SlideOut';
-import Input from '~/admin/components/FormField/Input/Input';
-import DateTimePicker from '~/admin/components/FormField/DateTimePicker/DateTimePicker';
+import NewAppointmentForm from '~/admin/components/NewAppointmentForm/NewAppoitmentForm';
 import { Text } from '~/utils/experimental';
-import { APIResponse } from '~/utils/fetch';
 import { store } from '~/store/store';
 import { card } from 'global.module.css';
-import { useAppointments } from '~/hooks/api/appointments';
-import { Appointment } from '~/types';
 
 export default function Dashboard() {
 	const dispatch = useDispatch(store);
@@ -73,68 +68,5 @@ export default function Dashboard() {
 				<NewAppointmentForm onSubmitComplete={closeSlideOut} />
 			</SlideOut>
 		</LayoutDefault>
-	);
-}
-
-type Fields = {
-	title: string;
-	datetime: string;
-};
-
-type FormProps = {
-	onSubmitComplete?: (
-		data: APIResponse<{
-			appointment: Appointment;
-			message: string;
-		}>
-	) => void;
-};
-
-function NewAppointmentForm({ onSubmitComplete }: FormProps) {
-	const { createAppointment } = useAppointments();
-	const {
-		control,
-		handleSubmit,
-		reset,
-		formState: { errors },
-	} = useForm<Fields>();
-
-	const onSubmit = async (formData: Fields) => {
-		console.log('formData', formData);
-		const data = await createAppointment(formData);
-
-		if (onSubmitComplete) {
-			onSubmitComplete(data);
-		}
-
-		reset();
-	};
-
-	return (
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<Input
-				control={control}
-				errors={errors}
-				name="title"
-				label="Title"
-				placeholder="Eg. Meeting with John Doe"
-				rules={{
-					required: true,
-				}}
-			/>
-
-			<div style={{ maxWidth: '300px' }}>
-				<DateTimePicker
-					control={control}
-					errors={errors}
-					name="datetime"
-					label="Date and Time"
-				/>
-			</div>
-
-			<Button type="submit" variant="primary">
-				Create Appointment
-			</Button>
-		</form>
 	);
 }
