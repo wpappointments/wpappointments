e5a78985b61562c034e117e914f7a6502d261d16
Dawid Urbański <hi@dawidurbanski.com>
2023-12-25 12:30:26 +0100

    Use redux store to manage slideouts

--- assets/scripts/store/selectors.ts
+++ assets/scripts/store/selectors.ts
@@ -1,7 +1,9 @@
 import { selectors as appointments } from './appointments/appointments';
 import { selectors as settings } from './settings/settings';
+import { selectors as slideouts } from './slideout/slideout';
 
 export default {
 	...settings,
 	...appointments,
+	...slideouts,
 };
