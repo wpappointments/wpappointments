9ef1dc5cd26868c1e041c42efdc1c9bc4dfd704e
Dawid Urbański <hi@dawidurbanski.com>
2024-01-30 11:01:50 +0100

    Run phpcs through tests directory

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,19 @@
 <?php
+/**
+ * Bootstrap the plugin unit testing environment.
+ *
+ * @package WPAppointments
+ */
 
 $wp_tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : sys_get_temp_dir();
 
 require_once $wp_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ *
+ * @since 0.0.1
+ */
 function _manually_load_plugin() {
 	$tests_dir  = __DIR__;
 	$plugin_dir = dirname( dirname( dirname( $tests_dir ) ) );
--- tests/wordpress/TestCase.php
+++ tests/wordpress/TestCase.php
@@ -1,6 +1,14 @@
 <?php
+/**
+ * WordPress PHPUnit test case class
+ *
+ * @package WPAppointments
+ */
 
 namespace TestsWP;
 
+/**
+ * Abstract WordPress PHPUnit test case class
+ */
 abstract class TestCase extends \WP_UnitTestCase {
 }
