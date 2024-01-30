792a38ed549a4a24553bed7ade0d0742b1aaae82
Dawid Urbański <hi@dawidurbanski.com>
2024-01-30 11:01:50 +0100

    Run phpcs through tests directory

--- src/Utils/Date.php
+++ src/Utils/Date.php
@@ -1,13 +1,29 @@
 <?php
+/**
+ * Date utility class
+ *
+ * @package WPAppointments
+ */
 
 namespace WPAppointments\Utils;
 
+/**
+ * Date utility class
+ */
 class Date {
-	public static function createDateRange( $start, $end ) {
-		$startDate = new \DateTime( $start );
-		$endDate   = new \DateTime( $end );
-		$interval  = new \DateInterval( 'P1D' );
-		$period    = new \DatePeriod( $startDate, $interval, $endDate );
+	/**
+	 * Create date range
+	 *
+	 * @param string $start Start date.
+	 * @param string $end End date.
+	 *
+	 * @return DatePeriod
+	 */
+	public static function create_date_range( $start, $end ) {
+		$start_date = new \DateTime( $start );
+		$end_date   = new \DateTime( $end );
+		$interval   = new \DateInterval( 'P1D' );
+		$period     = new \DatePeriod( $start_date, $interval, $end_date );
 		return $period;
 	}
 }
--- tests/php/Unit/DateTest.php
+++ tests/php/Unit/DateTest.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Date utility class
+ *
+ * @package WPAppointments
+ */
 
 namespace TestsPHP;
 
@@ -9,21 +14,21 @@ uses( \TestsPHP\TestCase::class );
 test(
 	'Date::createDateRange',
 	function () {
-		$startDate = '2020-01-01';
-		$endDate   = '2020-01-03';
-		$expected  = array(
+		$start_date = '2020-01-01';
+		$end_date   = '2020-01-03';
+		$expected   = array(
 			new \DateTime( '2020-01-01' ),
 			new \DateTime( '2020-01-02' ),
 			new \DateTime( '2020-01-03' ),
 		);
 
-		$period          = Date::createDateRange( $startDate, $endDate );
-		$recurrences     = $period->getRecurrences();
-		$periodStartDate = $period->getStartDate();
-		$periodEndDate   = $period->getEndDate();
+		$period            = Date::create_date_range( $start_date, $end_date );
+		$recurrences       = $period->getRecurrences();
+		$period_start_date = $period->getStartDate();
+		$period_end_date   = $period->getEndDate();
 
 		expect( $recurrences )->toBe( null );
-		expect( $periodStartDate )->toEqual( $expected[0] );
-		expect( $periodEndDate )->toEqual( $expected[2] );
+		expect( $period_start_date )->toEqual( $expected[0] );
+		expect( $period_end_date )->toEqual( $expected[2] );
 	}
 );
