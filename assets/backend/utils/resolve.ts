acb73e56c00075eba79a990421572bda443303f0
Dawid Urbański <hi@dawidurbanski.com>
2024-02-10 04:44:04 +0100

    Update all obsolete dependencies

--- assets/backend/utils/resolve.ts
+++ assets/backend/utils/resolve.ts
@@ -1,7 +1,7 @@
 import { Error } from './error';
 
 export default async function resolve<T>(
-	callback: () => Promise<any>
+	callback: () => Promise<any> // eslint-disable-line @typescript-eslint/no-explicit-any
 ): Promise<[Error | null, T | null]> {
 	try {
 		const response = await callback();
