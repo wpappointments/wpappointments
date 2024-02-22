57e8824cc0322d7125f2ba28e608369267c3a842
Dawid Urbański <hi@dawidurbanski.com>
2024-02-22 02:57:16 +0100

    Availability in the front end flow part 1

---
+++ assets/frontend/index.module.css.d.ts
@@ -0,0 +1,28 @@
+declare const styles:
+  & Readonly<{ "bookingFlow": string }>
+  & Readonly<{ "calendar": string }>
+  & Readonly<{ "calendarControls": string }>
+  & Readonly<{ "calendarControlButton": string }>
+  & Readonly<{ "calendarControlButton": string }>
+  & Readonly<{ "calendarControlButton": string }>
+  & Readonly<{ "calendarMonthHeader": string }>
+  & Readonly<{ "calendarHeader": string }>
+  & Readonly<{ "calendarHeaderDay": string }>
+  & Readonly<{ "calendarRow": string }>
+  & Readonly<{ "calendarDay": string }>
+  & Readonly<{ "calendarDay": string }>
+  & Readonly<{ "calendarDayAvailability": string }>
+  & Readonly<{ "calendarDayInCurrentMonth": string }>
+  & Readonly<{ "calendarDayInCurrentMonth": string }>
+  & Readonly<{ "calendarDaySelected": string }>
+  & Readonly<{ "daySlots": string }>
+  & Readonly<{ "daySlot": string }>
+  & Readonly<{ "daySlot": string }>
+  & Readonly<{ "timeSlotHeader": string }>
+  & Readonly<{ "input": string }>
+  & Readonly<{ "checkboxLabel": string }>
+  & Readonly<{ "checkbox": string }>
+  & Readonly<{ "error": string }>
+;
+export default styles;
+//# sourceMappingURL=./index.module.css.d.ts.map
--- src/Utils/Schedule.php
+++ src/Utils/Schedule.php
@@ -24,11 +24,14 @@ class Schedule {
 	 * @return DatePeriod
 	 */
 	public static function convert_schedule_to_date_range( $schedule, $date ) {
+		$timezone = wp_timezone_string();
 		$start = clone $date;
 		$start->setTime( (int) $schedule->start->hour, (int) $schedule->start->minute );
+		$start->setTimezone( new \DateTimeZone( $timezone ) );
 
 		$end = clone $date;
 		$end->setTime( (int) $schedule->end->hour, (int) $schedule->end->minute );
+		$end->setTimezone( new \DateTimeZone( $timezone ) );
 
 		$interval = new DateInterval( 'PT30M' );
 		$period   = new DatePeriod( $start, $interval, $end );
