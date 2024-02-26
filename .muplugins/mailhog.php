d57ac4a8d6ff4629b7f7a0a2df40ae034807b3b0
Dawid Urbański <hi@dawidurbanski.com>
2024-02-26 08:39:19 +0100

    Add mailhog setup for local email development and testing

---
+++ .muplugins/mailhog.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Plugin Name: Mailhog
+ * Version: 1.0
+ * License: GPL2
+ */
+
+add_filter('wp_mail_from', function() {
+  return 'contact@wpappointments.com';
+});
+
+add_filter('wp_mail_from_name', function() {
+  return 'WP Appointments';
+});
+
+add_action('phpmailer_init', function($phpmailer) {
+  $phpmailer->Host = 'mailhog';
+  $phpmailer->Port = 1025;
+  $phpmailer->IsSMTP();
+});
