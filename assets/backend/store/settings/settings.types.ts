95d7f9674bd234f41dcc2ae559ea8e9191817efc
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-01 19:30:49 +0100

    Use enum instead of string

--- assets/backend/store/settings/settings.types.ts
+++ assets/backend/store/settings/settings.types.ts
@@ -24,6 +24,7 @@ export type SettingsGeneral = {
 };
 
 export type SettingsAppointments = {
+	defaultStatus: 'confirmed' | 'pending';
 	defaultLength?: number;
 	timePickerPrecision?: number;
 	serviceId?: number;
