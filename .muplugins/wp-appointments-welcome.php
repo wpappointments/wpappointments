1cc5a1e70dde7ca99c8f7f5adaa3d3b982b1f2fa
Dawid Urbański <hi@dawidurbanski.com>
2024-03-07 05:18:58 +0100

    Welcome message: copy update. Hide activate if activated

--- .muplugins/wp-appointments-welcome.php
+++ .muplugins/wp-appointments-welcome.php
@@ -67,13 +67,39 @@ add_action( 'welcome_panel', function () {
                 </div>
             </div>
         </div>
-        <div class="welcome-panel-footer-wrapper">
-            <footer class="welcome-panel-footer">
-                <a href="<?php echo esc_url( $activate_url ); ?>" class="button button-primary">
-                    Activate WP Appointments
-                </a>
-            </footer>
+        <div class="welcome-panel-column-container-one-col">
+            <div class="welcome-panel-column">
+                <div class="welcome-panel-column-content">
+                    <h3>
+                        Demo plugin limitations
+                    </h3>
+                    <p>
+                        Please bear in mind that this is a beta version, and some features are still in the works.
+                    </p>
+                    <p>
+                        Everything you see here is a work in progress and subject to change. We're working hard to make WP Appointments the best it can be, and we appreciate your patience and support.
+                    </p>
+                    <ul class="welcome-panel-list">
+                        <li>🚫 No extensions available at this point, including: services, employees, locations or calendar sync</li>
+                        <li>🚫 Admin calendar view - only month view</li>
+                        <li>🚫 Limited number of options in the frontent booking flow Gutenberg block</li>
+                        <li>🚫 No shortocode builder available yet. Only gutenberg block</li>
+                        <li>🚫 Email notifications are not yet editable or customizable</li>
+                        <li>🚫 Only simple opening hours schedule available. No days off or holiday settings</li>
+                        <li>🚫 No stats or reports available yet</li>
+                    </ul>
+                </div>
+            </div>
         </div>
+        <?php if ( !is_plugin_active( $plugin_file ) ) : ?>
+          <div class="welcome-panel-footer-wrapper">
+              <footer class="welcome-panel-footer">
+                  <a href="<?php echo esc_url( $activate_url ); ?>" class="button button-primary">
+                      Activate WP Appointments
+                  </a>
+              </footer>
+          </div>
+        <?php endif; ?>
     </div>
 	<?php
 } );
@@ -118,20 +144,20 @@ add_action( 'admin_head', function () {
         max-width: var(--max-width);
         margin-inline: auto;
         padding: 48px;
+      }
 
-        &::before {
-          position: absolute;
-          top: 0;
-          right: 0;
-          height: 100%;
-          width: 50%;
-          content: '';
-          opacity: 0.2;
-          background-image: url("data:image/svg+xml,%3Csvg id='uuid-108c8467-2f60-4f27-9cc3-731fecd27c8f' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 51.44 51.44'%3E%3Cg id='uuid-0e84affd-ba30-440d-9b7b-52f64fe4c4f3'%3E%3Cpath d='m25.72,0C11.54,0,0,11.54,0,25.72s11.54,25.72,25.72,25.72,25.72-11.54,25.72-25.72S39.9,0,25.72,0Zm0,40.04c-7.9,0-14.33-6.43-14.33-14.32s6.43-14.33,14.33-14.33,14.33,6.43,14.33,14.33-6.43,14.32-14.33,14.32Z' fill='%23ffffff'/%3E%3Crect x='20.39' y='19.56' width='14.35' height='9.09' transform='translate(-8.97 26.55) rotate(-45)' fill='%23ffffff'/%3E%3C/g%3E%3C/svg%3E");
-          background-repeat: no-repeat;
-          background-size: 75%;
-          background-position: calc(100% - 20px) 20px;
-        }
+      .welcome-panel-header::before {
+        position: absolute;
+        top: 0;
+        right: 0;
+        height: 100%;
+        width: 50%;
+        content: '';
+        opacity: 0.2;
+        background-image: url("data:image/svg+xml,%3Csvg id='uuid-108c8467-2f60-4f27-9cc3-731fecd27c8f' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 51.44 51.44'%3E%3Cg id='uuid-0e84affd-ba30-440d-9b7b-52f64fe4c4f3'%3E%3Cpath d='m25.72,0C11.54,0,0,11.54,0,25.72s11.54,25.72,25.72,25.72,25.72-11.54,25.72-25.72S39.9,0,25.72,0Zm0,40.04c-7.9,0-14.33-6.43-14.33-14.32s6.43-14.33,14.33-14.33,14.33,6.43,14.33,14.33-6.43,14.32-14.33,14.32Z' fill='%23ffffff'/%3E%3Crect x='20.39' y='19.56' width='14.35' height='9.09' transform='translate(-8.97 26.55) rotate(-45)' fill='%23ffffff'/%3E%3C/g%3E%3C/svg%3E");
+        background-repeat: no-repeat;
+        background-size: 75%;
+        background-position: calc(100% - 20px) 20px;
       }
 
       .welcome-panel-header h2 {
@@ -145,6 +171,14 @@ add_action( 'admin_head', function () {
       .welcome-panel .welcome-panel-column-container {
         max-width: var(--max-width);
         margin-inline: auto;
+      }
+
+      .welcome-panel-column-container-one-col {
+        width: var(--max-width);
+        box-sizing: border-box;
+        max-width: 100%;
+        margin-inline: auto;
+        padding: 0 32px 32px;
       }
 
       .welcome-panel-column-content {
@@ -269,6 +303,10 @@ add_action( 'admin_head', function () {
           padding: 24px 48px;
         }
 
+        .welcome-panel-column-container-one-col {
+          padding: 0 48px 48px;
+        }
+
         .drawer {
           max-width: 60%;
         }
@@ -285,21 +323,21 @@ add_action( 'admin_head', function () {
 
 
 add_action( 'admin_bar_menu', function ( $wp_admin_bar ) {
-	$args = array(
-		'id'     => 'wp-appointments-feedback',
-		'title'  => 'Send Feedback',
-		'href'   => '#',
-		'parent' => 'top-secondary',
-		'meta'   => array(
-			'class' => 'wp-appointments-feedback js-feedback-link',
-			'title' => 'Send Feedback',
-		),
-	);
-	$wp_admin_bar->add_node( $args );
+  $args = array(
+      'id'     => 'wp-appointments-feedback',
+      'title'  => 'Send Feedback',
+      'href'   => '#',
+      'parent' => 'top-secondary',
+      'meta'   => array(
+        'class' => 'wp-appointments-feedback js-feedback-link',
+        'title' => 'Send Feedback',
+      ),
+  );
+  $wp_admin_bar->add_node( $args );
 } );
 
 add_action( 'admin_footer', function () {
-	?>
+    ?>
     <div class="drawer js-feedback-drawer">
         <button type="button" class="drawer-close js-feedback-link">
             Close
@@ -316,7 +354,8 @@ add_action( 'admin_footer', function () {
                 if ("undefined" != typeof Tally) v(); else if (d.querySelector('script[src="' + w + '"]') == null) {
                     var s = d.createElement("script");
                     s.src = w, s.onload = v, s.onerror = v, d.body.appendChild(s);
-                }</script>
+                }
+            </script>
         </div>
     </div>
     <script type="module">
@@ -339,5 +378,5 @@ add_action( 'admin_footer', function () {
             });
         })(window.jQuery);
     </script>
-	<?php
+    <?php
 } );
