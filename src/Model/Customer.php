4d119d8c0704b8a08a94abb0da65cdb65a428dd5
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-08 15:13:34 +0100

    Revert "Update customers creation logic"
    This reverts commit 3716e0b4ce010922961379242aefaefcc7997a72.

--- src/Model/Customer.php
+++ src/Model/Customer.php
@@ -21,44 +21,24 @@ class Customer {
 	 * @return int|\WP_Error
 	 */
 	public function create( $customer, $password = null ) {
-		$email          = sanitize_text_field( wp_unslash( $customer->email ) );
-		$name           = sanitize_user( wp_unslash( $customer->name ), true );
-		$create_account = sanitize_text_field( wp_unslash( $customer->create_account ) );
-		$user_id        = null;
+		$email = sanitize_text_field( wp_unslash( $customer->email ), true );
+		$name  = sanitize_user( wp_unslash( $customer->name ), true );
 
-		if ( $create_account ) {
-			$user_id = wp_insert_user(
-				array(
-					'user_login'   => $email ?: $name,
-					'user_pass'    => $password ?? wp_generate_password(),
-					'user_email'   => $email,
-					'display_name' => $name,
-					'role'         => 'wpa-customer',
-				)
-			);
-		}
-
-		$post_id = wp_insert_post(
+		$user_id = wp_insert_user(
 			array(
-				'post_type'   => 'wpa_customer',
-				'post_status' => 'publish',
-				'post_title'  => $email ?: $name,
+				'user_login'   => $email ?: $name,
+				'user_pass'    => $password ?? wp_generate_password(),
+				'user_email'   => $email,
+				'display_name' => $name,
+				'role'         => 'wpa-customer',
 			)
 		);
-
-		if ( is_wp_error( $post_id ) || $post_id === 0 ) {
-			return new \WP_Error( 'wpa_customer_cpt_create_error', __( 'There was a problem with saving the customer.', 'wpappointments' ) );
-		}
 
 		if ( is_wp_error( $user_id ) ) {
 			return $user_id;
 		}
 
 		update_user_meta( $user_id, 'phone', sanitize_text_field( $customer->phone ) );
-
-		if ( $post_id && $user_id ) {
-			update_post_meta( $post_id, 'user_id', $user_id );
-		}
 
 		return $user_id;
 	}
