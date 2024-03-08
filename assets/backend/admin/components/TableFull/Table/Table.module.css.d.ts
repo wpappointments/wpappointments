194b489a6cfb6b1a86327abf191bcc5f7582523a
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-08 19:01:26 +0100

    Add Table component

---
+++ assets/backend/admin/components/TableFull/Table/Table.module.css.d.ts
@@ -0,0 +1,8 @@
+declare const styles:
+  & Readonly<{ "table": string }>
+  & Readonly<{ "table": string }>
+  & Readonly<{ "table": string }>
+  & Readonly<{ "table": string }>
+;
+export default styles;
+//# sourceMappingURL=./Table.module.css.d.ts.map
