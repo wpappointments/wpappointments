0176b9bf98c9cf557ae2a598cb036db026fdbdd7
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-08 14:24:39 +0100

    Don't labels without values

--- assets/backend/admin/components/CustomerSelector/Summary/Summary.tsx
+++ assets/backend/admin/components/CustomerSelector/Summary/Summary.tsx
@@ -1,8 +1,9 @@
 import { ReactNode } from 'react';
-import { Card, CardHeader, CardBody } from '@wordpress/components';
+import { Card, CardBody, CardHeader } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { Customer } from '~/backend/store/customers/customers.types';
 import styles from './Summary.module.css';
+
 
 export type SummaryProps = {
 	customer: Customer;
@@ -20,14 +21,24 @@ export default function Summary({ customer, headerActions }: SummaryProps) {
 					<div className={styles.summaryCustomerName}>
 						<strong>{customer.name}</strong>
 					</div>
+					{(customer.email || customer.phone) && (
 					<span>
-						mail:{' '}
-						<a href={`mailto:${customer.email}`} target="_blank">
-							{customer.email}
-						</a>{' '}
-						| phone:{' '}
-						<a href={`tel:${customer.phone}`}>{customer.phone}</a>
+						{customer.email && (
+							<>
+								mail:{' '}
+								<a href={`mailto:${customer.email}`} target="_blank">
+									{customer.email}
+								</a>{' '}|{' '}
+							</>
+						)}
+						{customer.phone && (
+							<>
+								phone:{' '}
+								<a href={`tel:${customer.phone}`}>{customer.phone}</a>
+							</>
+						)}
 					</span>
+					)}
 				</div>
 			</CardBody>
 		</Card>
