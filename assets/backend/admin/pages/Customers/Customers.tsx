53351d93b88c112fc1f1e2bb43e1d5f3c0566dec
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-08 13:06:43 +0100

    Add basic Customers page

--- assets/backend/admin/pages/Customers/Customers.tsx
+++ assets/backend/admin/pages/Customers/Customers.tsx
@@ -1,9 +1,39 @@
+import { Button, Card, CardHeader } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+import { Text } from '~/backend/utils/experimental';
+import useSlideout from '~/backend/hooks/useSlideout';
+import CardBody from '~/backend/admin/components/CardBody/CardBody';
 import LayoutDefault from '~/backend/admin/layouts/LayoutDefault/LayoutDefault';
+import globalStyles from 'global.module.css';
 
 export default function Customers() {
-    return (
-        <LayoutDefault title="Settings">
-            Hello, Customers!
-        </LayoutDefault>
-    );
+	const { openSlideOut, isSlideoutOpen } = useSlideout();
+
+	return (
+		<LayoutDefault title="Settings">
+			<Card className={globalStyles.card}>
+				<CardHeader>
+					<Text size="title">
+						{__('All Customers', 'wpappointments')}
+					</Text>
+					<Button
+						variant="primary"
+						onClick={() => {
+							openSlideOut({
+								id: 'customer',
+								data: {
+									mode: 'create',
+								},
+							});
+						}}
+					>
+						{__('Create New Customer', 'wpappointments')}
+					</Button>
+				</CardHeader>
+				<CardBody>
+					
+				</CardBody>
+			</Card>
+		</LayoutDefault>
+	);
 }
