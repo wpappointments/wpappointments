3716e0b4ce010922961379242aefaefcc7997a72
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-08 14:26:43 +0100

    Update customers creation logic
    For both registered and anonymous create w wpa_customer entry. If create account selected also create a real user account with wpa-customer role.

--- src/Model/Customer.php
+++ src/Model/Customer.php
@@ -21,24 +21,44 @@ class Customer {
 	 * @return int|\WP_Error
 	 */
 	public function create( $customer, $password = null ) {
-		$email = sanitize_text_field( wp_unslash( $customer->email ), true );
-		$name  = sanitize_user( wp_unslash( $customer->name ), true );
+		$email          = sanitize_text_field( wp_unslash( $customer->email ) );
+		$name           = sanitize_user( wp_unslash( $customer->name ), true );
+		$create_account = sanitize_text_field( wp_unslash( $customer->create_account ) );
+		$user_id        = null;
 
-		$user_id = wp_insert_user(
+		if ( $create_account ) {
+			$user_id = wp_insert_user(
+				array(
+					'user_login'   => $email ?: $name,
+					'user_pass'    => $password ?? wp_generate_password(),
+					'user_email'   => $email,
+					'display_name' => $name,
+					'role'         => 'wpa-customer',
+				)
+			);
+		}
+
+		$post_id = wp_insert_post(
 			array(
-				'user_login'   => $email ?: $name,
-				'user_pass'    => $password ?? wp_generate_password(),
-				'user_email'   => $email,
-				'display_name' => $name,
-				'role'         => 'wpa-customer',
+				'post_type'   => 'wpa_customer',
+				'post_status' => 'publish',
+				'post_title'  => $email ?: $name,
 			)
 		);
+
+		if ( is_wp_error( $post_id ) || $post_id === 0 ) {
+			return new \WP_Error( 'wpa_customer_cpt_create_error', __( 'There was a problem with saving the customer.', 'wpappointments' ) );
+		}
 
 		if ( is_wp_error( $user_id ) ) {
 			return $user_id;
 		}
 
 		update_user_meta( $user_id, 'phone', sanitize_text_field( $customer->phone ) );
+
+		if ( $post_id && $user_id ) {
+			update_post_meta( $post_id, 'user_id', $user_id );
+		}
 
 		return $user_id;
 	}
