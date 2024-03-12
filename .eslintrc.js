b00d5de0bd0023d3949c7fc0515b97d9ebe180bd
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-12 20:03:45 +0100

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,4 +2,7 @@ module.exports = {
 	root: true,
 	extends: [ 'plugin:@typescript-eslint/recommended' ],
 	plugins: [ '@typescript-eslint', 'import' ],
+	rules: {
+		'@typescript-eslint/ban-ts-comment': 'off',
+	},
 };
