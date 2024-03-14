270b3c10809440d65c1ae28f1ccbedb3f05fece8
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-14 19:28:40 +0100

    Fetch all appointments

--- assets/backend/admin/pages/Calendar/Calendar.tsx
+++ assets/backend/admin/pages/Calendar/Calendar.tsx
@@ -100,7 +100,7 @@ export default function Calendar() {
 	const { openSlideOut, isSlideoutOpen } = useSlideout();
 	const { appointments } = useSelect(() => {
 		return select(store).getAppointments({
-			posts_per_page: 100,
+			posts_per_page: -1,
 		});
 	}, []);
 
