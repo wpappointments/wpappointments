a66131a094095e87e202460cb1d7a47a9d518575
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-14 14:01:46 +0100

    Remove empty line

--- assets/backend/store/customers/customers.ts
+++ assets/backend/store/customers/customers.ts
@@ -6,7 +6,6 @@ import { FetchFromApiActionReturn, baseActions } from '../actions';
 import { type State } from '../store';
 import { type CustomersState } from './customers.types';
 
-
 type Action = ReturnType<(typeof actions)[keyof typeof actions]>;
 type Query = Record<string, any>;
 type Response = APIResponse<{
