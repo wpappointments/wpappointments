256ec92643f07046257fc191e31ac3aab0fa068b
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-15 17:02:00 +0100

    Remove unlock file

--- assets/backend/admin/components/DataViews/unlock.js
+++
@@ -1,7 +0,0 @@
-import { __dangerousOptInToUnstableAPIsOnlyForCoreModules } from '@wordpress/components/src/private-apis';
-
-export const { lock, unlock } =
-	__dangerousOptInToUnstableAPIsOnlyForCoreModules(
-		'I know using unstable features means my theme or plugin will inevitably break in the next version of WordPress.',
-		'@wordpress/components'
-	);
