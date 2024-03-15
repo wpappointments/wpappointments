e66f17cf3105b931201af14f34571d4fa65ee4e9
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-15 17:14:49 +0100

    Remove obsolete component

--- assets/backend/admin/components/TableFull/Table/Table.module.css.d.ts
+++
@@ -1,8 +0,0 @@
-declare const styles:
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-  & Readonly<{ "table": string }>
-;
-export default styles;
-//# sourceMappingURL=./Table.module.css.d.ts.map
