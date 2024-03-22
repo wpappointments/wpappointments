cf8b78284c90c115b7100ada32f28cf61da62d37
Marcin Krzemiński <marcin@krzeminski.net>
2024-03-22 15:03:04 +0100

    Update release script

--- .scripts/release.sh
+++ .scripts/release.sh
@@ -4,16 +4,18 @@ version=$(grep -o "Version: [0-9.]*" wpappointments.php | awk '{print $2}')
 
 echo "Releasing version $version"
 
-if [ -f wpappointments.zip ]; then
-    rm wpappointments.zip
-fi
-
 pnpm build
 
 mv vendor vendor-temp
 composer install --no-dev > /dev/null 2>&1
 
-zip -qr wpappointments.zip . -x@.zipignore
+mkdir "wpappointments"
+
+rsync -av --exclude=wpappointments --exclude-from=.zipignore . wpappointments
+
+zip -qr "wpappointments-$version.zip" wpappointments/* -x@.zipignore
+
+rm -rf "wpappointments"
 
 rm -rf vendor
 mv vendor-temp vendor
