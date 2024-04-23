365efd74e56539e0ceba10ed64dd7ddc7b74ae4c
Dawid Urbański <hi@dawidurbanski.com>
2024-04-24 00:07:05 +0200

    Add test to verify getting customers is only allowed for authenticated users

--- plugins/wpappointments/tests/php/Api/CustomerTest.php
+++ plugins/wpappointments/tests/php/Api/CustomerTest.php
@@ -10,7 +10,7 @@ namespace Tests\Api;
 uses( \TestTools\RestTestCase::class );
 
 test(
-  'GET wpappointments/v1/customer',
+  'GET wpappointments/v1/customer - status 200',
   function() {
     wp_set_current_user( 1 );
 
@@ -36,3 +36,26 @@ test(
     $this->assertCount( 5, $data['data']->customers );
   }
 );
+
+test(
+  'GET wpappointments/v1/customer - status 401 unauthorized',
+  function() {
+    $user_id = $this->factory()->user->create( [
+      'role' => 'wpa-customer',
+      'meta_input' => [
+        'phone' => '12345'
+      ]
+    ] );
+    wp_set_current_user( $user_id );
+
+    $results = $this->do_rest_get_request( 'customer' );
+    $is_error = $results->is_error();
+    $status = $results->get_status();
+    $data = $results->get_data();
+
+    $this->assertInstanceOf( \WP_REST_Response::class, $results );
+    $this->assertTrue( $is_error );
+    $this->assertEquals( 401, $status );
+    $this->assertEquals( "rest_forbidden", $data['type'] );
+  }
+)->only();
