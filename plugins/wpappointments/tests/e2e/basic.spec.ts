43e3ab23a570f6279fa8540f8d60b2f3b2572de9
dawidurbanski <hi@dawidurbanski.com>
2026-03-15 20:17:00 +0100

    Run pnpm format:js

--- plugins/wpappointments/tests/e2e/basic.spec.ts
+++ plugins/wpappointments/tests/e2e/basic.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:8889/wp-login.php'); // Assuming default wp-env port
+	await page.goto('http://localhost:8889/wp-login.php'); // Assuming default wp-env port
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Log In/);
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Log In/);
 });
